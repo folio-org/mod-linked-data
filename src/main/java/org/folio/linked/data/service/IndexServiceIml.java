6ccd5e933031341247a998e8073ca1a00934a4c2
Asadbek_Muminov <asadbek_muminov@epam.com>
2023-09-06 16:33:05 +0500

    MODLD-95 fixed checkstyle issues

--- src/main/java/org/folio/linked/data/service/IndexServiceIml.java
+++ src/main/java/org/folio/linked/data/service/IndexServiceIml.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class IndexServiceIml implements IndexService{
+public class IndexServiceIml implements IndexService {
 
   private final ResourceRepository resourceRepository;
 
