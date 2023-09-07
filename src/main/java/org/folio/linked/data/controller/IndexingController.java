939d6738ed790453f02c9420456070bae0a422f5
Asadbek_Muminov <asadbek_muminov@epam.com>
2023-09-07 06:13:18 +0500

    MODLD-95 fixed checkstyle

--- src/main/java/org/folio/linked/data/controller/IndexingController.java
+++ src/main/java/org/folio/linked/data/controller/IndexingController.java
@@ -1,16 +1,20 @@
 package org.folio.linked.data.controller;
 
+import static org.folio.linked.data.util.Constants.SEARCH_PROFILE;
+
 import lombok.RequiredArgsConstructor;
 import org.folio.linked.data.domain.dto.IndexRequest;
 import org.folio.linked.data.domain.dto.IndexResponse;
 import org.folio.linked.data.rest.resource.IndexApi;
 import org.folio.linked.data.service.IndexService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
+@Profile(SEARCH_PROFILE)
 @RequiredArgsConstructor
 public class IndexingController implements IndexApi {
 
