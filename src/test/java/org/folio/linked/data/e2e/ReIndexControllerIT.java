15eb34ad8c0006ae3f610a9ff38b238ec72ec613
Asadbek_Muminov <asadbek_muminov@epam.com>
2023-09-15 20:10:39 +0500

    MODLD-95 fixed checkstyle

--- src/test/java/org/folio/linked/data/e2e/ReIndexControllerIT.java
+++ src/test/java/org/folio/linked/data/e2e/ReIndexControllerIT.java
@@ -85,11 +85,13 @@ class ReIndexControllerIT {
     Resource resource1 = bibframeMapper.toEntity(bibframeRequest1);
     resourceRepo.save(resource1);
 
-    BibframeRequest bibframeRequest2 = objectMapper.readValue(getBibframeSampleTest("77 mm"), BibframeRequest.class);
+    BibframeRequest bibframeRequest2 =
+      objectMapper.readValue(getBibframeSampleTest("77 mm"), BibframeRequest.class);
     Resource resource2 = bibframeMapper.toEntity(bibframeRequest2);
     resourceRepo.save(resource2);
 
-    return resourceRepo.findResourcesByTypeFull(Set.of(BibframeConstants.INSTANCE, BibframeConstants.MONOGRAPH), Pageable.ofSize(10000))
+    return resourceRepo.findResourcesByTypeFull(
+      Set.of(BibframeConstants.INSTANCE, BibframeConstants.MONOGRAPH), Pageable.ofSize(10000))
       .getContent();
   }
 }
