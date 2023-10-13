e12d78afd1d09aae7862672ce9e0152d7a2eadd6
pbobylev <pavel_bobylev@epam.com>
2023-10-13 16:57:50 +0600

    MODLD-89: adaptation for new search dto

--- src/main/java/org/folio/linked/data/mapper/resource/kafka/KafkaMessageMapperImpl.java
+++ src/main/java/org/folio/linked/data/mapper/resource/kafka/KafkaMessageMapperImpl.java
@@ -1,18 +1,23 @@
 package org.folio.linked.data.mapper.resource.kafka;
 
+import static java.util.Arrays.stream;
+import static java.util.Objects.nonNull;
 import static org.folio.linked.data.util.BibframeConstants.DATE;
 import static org.folio.linked.data.util.BibframeConstants.EAN_VALUE;
 import static org.folio.linked.data.util.BibframeConstants.EDITION_STATEMENT;
 import static org.folio.linked.data.util.BibframeConstants.INSTANCE;
+import static org.folio.linked.data.util.BibframeConstants.INSTANCE_TITLE_PRED;
 import static org.folio.linked.data.util.BibframeConstants.LOCAL_ID_VALUE;
+import static org.folio.linked.data.util.BibframeConstants.MAIN_TITLE;
 import static org.folio.linked.data.util.BibframeConstants.MAP_PRED;
 import static org.folio.linked.data.util.BibframeConstants.NAME;
 import static org.folio.linked.data.util.BibframeConstants.PROVIDER_DATE;
 import static org.folio.linked.data.util.BibframeConstants.PUBLICATION_PRED;
+import static org.folio.linked.data.util.BibframeConstants.SUBTITLE;
+import static org.folio.search.domain.dto.BibframeIdentifiersInner.TypeEnum;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import lombok.NonNull;
 import org.folio.linked.data.exception.NotSupportedException;
@@ -23,6 +28,7 @@ import org.folio.search.domain.dto.BibframeContributorsInner;
 import org.folio.search.domain.dto.BibframeIdentifiersInner;
 import org.folio.search.domain.dto.BibframeIndex;
 import org.folio.search.domain.dto.BibframePublicationsInner;
+import org.folio.search.domain.dto.BibframeTitlesInner;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -32,7 +38,7 @@ public class KafkaMessageMapperImpl implements KafkaMessageMapper {
   public BibframeIndex toIndex(@NonNull Resource resource) {
     var instance = extractInstance(resource);
     var bibframeIndex = new BibframeIndex(resource.getResourceHash().toString());
-    bibframeIndex.setTitle(instance.getLabel());
+    bibframeIndex.setTitles(extractTitles(instance));
     bibframeIndex.setIdentifiers(extractIdentifiers(instance));
     bibframeIndex.setContributors(extractContributors(instance));
     bibframeIndex.setPublications(extractPublications(instance));
@@ -50,19 +56,34 @@ public class KafkaMessageMapperImpl implements KafkaMessageMapper {
           + "is no Instance found"));
   }
 
+  private List<BibframeTitlesInner> extractTitles(Resource resource) {
+    return resource.getOutgoingEdges().stream()
+      .filter(re -> INSTANCE_TITLE_PRED.equals(re.getPredicate().getLabel()))
+      .map(ResourceEdge::getTarget)
+      .flatMap(t -> {
+        var titles = new ArrayList<BibframeTitlesInner>();
+        addTitle(t, MAIN_TITLE, titles, BibframeTitlesInner.TypeEnum.MAIN);
+        addTitle(t, SUBTITLE, titles, BibframeTitlesInner.TypeEnum.SUB);
+        return titles.stream();
+      })
+      .toList();
+  }
+
+  private void addTitle(Resource t, String field, List<BibframeTitlesInner> titles,
+                        BibframeTitlesInner.TypeEnum type) {
+    if (nonNull(getValue(t.getDoc(), field))) {
+      titles.add(new BibframeTitlesInner()
+        .value(getValue(t.getDoc(), field))
+        .type(type));
+    }
+  }
+
   private List<BibframeIdentifiersInner> extractIdentifiers(Resource resource) {
     return resource.getOutgoingEdges().stream()
       .filter(re -> MAP_PRED.equals(re.getPredicate().getLabel()))
-      .filter(re -> {
-        var typeUri = re.getTarget().getFirstType().getTypeUri();
-        return Arrays.stream(BibframeIdentifiersInner.TypeEnum.values())
-          .anyMatch(typeEnum -> typeUri.contains(typeEnum.getValue())
-            // to be removed after mod-search-ld DTO alignment
-            || typeUri.contains("Ean")
-            || typeUri.contains("LocalId")
-            || typeUri.contains("UNKNOWN"));
-      })
       .map(ResourceEdge::getTarget)
+      .filter(r -> stream(TypeEnum.values())
+        .anyMatch(typeEnum -> r.getFirstType().getTypeUri().contains(typeEnum.getValue())))
       .map(ir -> new BibframeIdentifiersInner()
         .value(getValue(ir.getDoc(), NAME, EAN_VALUE, LOCAL_ID_VALUE))
         .type(toTypeEnum(ir.getFirstType())))
@@ -72,21 +93,16 @@ public class KafkaMessageMapperImpl implements KafkaMessageMapper {
   private String getValue(JsonNode doc, String... values) {
     for (String value : values) {
       if (doc.has(value)) {
-        return doc.get(value).get(0).textValue();
+        return doc.get(value).get(0).asText();
       }
     }
     return null;
   }
 
-  private BibframeIdentifiersInner.TypeEnum toTypeEnum(ResourceType resourceType) {
+  private TypeEnum toTypeEnum(ResourceType resourceType) {
     var typeUri = resourceType.getTypeUri();
     var extractedTypeWord = typeUri.substring(typeUri.lastIndexOf("/") + 1);
-    var reworded = extractedTypeWord
-      // to be removed after mod-search-ld DTO alignment
-      .replace("Ean", "EAN")
-      .replace("LocalId", "Local")
-      .replace("UNKNOWN", "Other");
-    return BibframeIdentifiersInner.TypeEnum.fromValue(reworded);
+    return TypeEnum.fromValue(extractedTypeWord);
   }
 
   private List<BibframeContributorsInner> extractContributors(Resource resource) {
