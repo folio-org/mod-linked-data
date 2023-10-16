29d60d212b41d8a4feed0dcdb86413b2fb4d91bc
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2023-10-16 15:09:38 +0600

    MODLD-89: adaptation for new search dto (#76)
    * working
    
    * MODLD-96: fix provider date field
    
    * MODLD-89: adaptation for new search dto

--- src/test/java/org/folio/linked/data/mapper/resource/kafka/KafkaMessageMapperTest.java
+++ src/test/java/org/folio/linked/data/mapper/resource/kafka/KafkaMessageMapperTest.java
@@ -4,15 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.linked.data.test.TestUtil.getJsonNode;
 import static org.folio.linked.data.test.TestUtil.randomLong;
 import static org.folio.linked.data.util.BibframeConstants.DATE;
+import static org.folio.linked.data.util.BibframeConstants.EAN;
+import static org.folio.linked.data.util.BibframeConstants.EAN_VALUE;
 import static org.folio.linked.data.util.BibframeConstants.EDITION_STATEMENT;
 import static org.folio.linked.data.util.BibframeConstants.INSTANCE;
+import static org.folio.linked.data.util.BibframeConstants.INSTANCE_TITLE;
+import static org.folio.linked.data.util.BibframeConstants.INSTANCE_TITLE_PRED;
 import static org.folio.linked.data.util.BibframeConstants.ISBN;
+import static org.folio.linked.data.util.BibframeConstants.LCCN;
+import static org.folio.linked.data.util.BibframeConstants.LOCAL_ID;
+import static org.folio.linked.data.util.BibframeConstants.LOCAL_ID_VALUE;
+import static org.folio.linked.data.util.BibframeConstants.MAIN_TITLE;
 import static org.folio.linked.data.util.BibframeConstants.MAP_PRED;
 import static org.folio.linked.data.util.BibframeConstants.NAME;
+import static org.folio.linked.data.util.BibframeConstants.OTHER_ID;
+import static org.folio.linked.data.util.BibframeConstants.PARALLEL_TITLE;
 import static org.folio.linked.data.util.BibframeConstants.PROVIDER_EVENT;
 import static org.folio.linked.data.util.BibframeConstants.PUBLICATION_PRED;
+import static org.folio.linked.data.util.BibframeConstants.SUBTITLE;
+import static org.folio.linked.data.util.BibframeConstants.VARIANT_TITLE;
+import static org.folio.search.domain.dto.BibframeIdentifiersInner.TypeEnum;
+import static org.folio.search.domain.dto.BibframeTitlesInner.TypeEnum.MAIN;
+import static org.folio.search.domain.dto.BibframeTitlesInner.TypeEnum.SUB;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -22,6 +38,7 @@ import org.folio.linked.data.model.entity.Resource;
 import org.folio.linked.data.model.entity.ResourceEdge;
 import org.folio.linked.data.model.entity.ResourceType;
 import org.folio.search.domain.dto.BibframeIdentifiersInner;
+import org.folio.search.domain.dto.BibframeTitlesInner;
 import org.folio.spring.test.type.UnitTest;
 import org.junit.jupiter.api.Test;
 
@@ -63,16 +80,27 @@ class KafkaMessageMapperTest {
     instance.setResourceHash(randomLong());
     instance.setLabel(UUID.randomUUID().toString());
     instance.addType(new ResourceType().setTypeUri(INSTANCE));
-    var identifier = new Resource();
-    identifier.setResourceHash(randomLong());
-    identifier.setDoc(getJsonNode(Map.of(NAME, List.of(randomLong()))));
-    identifier.addType(new ResourceType().setTypeUri(ISBN));
-    instance.getOutgoingEdges().add(new ResourceEdge(instance, identifier, new Predicate(MAP_PRED)));
-    var publication = new Resource();
-    publication.setResourceHash(randomLong());
-    publication.setDoc(getJsonNode(Map.of(DATE, List.of("2023"), NAME, List.of(UUID.randomUUID().toString()))));
-    publication.addType(new ResourceType().setSimpleLabel(PROVIDER_EVENT));
-    instance.getOutgoingEdges().add(new ResourceEdge(instance, publication, new Predicate(PUBLICATION_PRED)));
+    var title1 = getTitle(INSTANCE_TITLE);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, title1, new Predicate(INSTANCE_TITLE_PRED)));
+    var title2 = getTitle(PARALLEL_TITLE);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, title2, new Predicate(INSTANCE_TITLE_PRED)));
+    var title3 = getTitle(VARIANT_TITLE);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, title3, new Predicate(INSTANCE_TITLE_PRED)));
+    var isbn = getIdentifier(NAME, ISBN);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, isbn, new Predicate(MAP_PRED)));
+    var lccn = getIdentifier(NAME, LCCN);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, lccn, new Predicate(MAP_PRED)));
+    var ean = getIdentifier(EAN_VALUE, EAN);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, ean, new Predicate(MAP_PRED)));
+    var localId = getIdentifier(LOCAL_ID_VALUE, LOCAL_ID);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, localId, new Predicate(MAP_PRED)));
+    var otherId = getIdentifier(NAME, OTHER_ID);
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, otherId, new Predicate(MAP_PRED)));
+    var pub = new Resource();
+    pub.setResourceHash(randomLong());
+    pub.setDoc(getJsonNode(Map.of(DATE, List.of("2023"), NAME, List.of(UUID.randomUUID().toString()))));
+    pub.addType(new ResourceType().setSimpleLabel(PROVIDER_EVENT));
+    instance.getOutgoingEdges().add(new ResourceEdge(instance, pub, new Predicate(PUBLICATION_PRED)));
     instance.setDoc(getJsonNode(Map.of(EDITION_STATEMENT, List.of(UUID.randomUUID().toString()))));
 
     // when
@@ -80,15 +108,51 @@ class KafkaMessageMapperTest {
 
     // then
     assertThat(result.getId()).isEqualTo(instance.getResourceHash().toString());
-    assertThat(result.getTitle()).isEqualTo(instance.getLabel());
-    assertThat(result.getIdentifiers().get(0).getValue()).isEqualTo(
-      identifier.getDoc().get(NAME).get(0).textValue());
-    assertThat(result.getIdentifiers().get(0).getType()).isEqualTo(BibframeIdentifiersInner.TypeEnum.ISBN);
+    assertThat(result.getTitles()).hasSize(6);
+    assertTitle(result.getTitles().get(0), title1.getDoc().get(MAIN_TITLE), MAIN);
+    assertTitle(result.getTitles().get(1), title1.getDoc().get(SUBTITLE), SUB);
+    assertTitle(result.getTitles().get(2), title2.getDoc().get(MAIN_TITLE), MAIN);
+    assertTitle(result.getTitles().get(3), title2.getDoc().get(SUBTITLE), SUB);
+    assertTitle(result.getTitles().get(4), title3.getDoc().get(MAIN_TITLE), MAIN);
+    assertTitle(result.getTitles().get(5), title3.getDoc().get(SUBTITLE), SUB);
+    assertThat(result.getIdentifiers()).hasSize(5);
+    assertId(result.getIdentifiers().get(0), isbn.getDoc().get(NAME), TypeEnum.ISBN);
+    assertId(result.getIdentifiers().get(1), lccn.getDoc().get(NAME), TypeEnum.LCCN);
+    assertId(result.getIdentifiers().get(2), ean.getDoc().get(EAN_VALUE), TypeEnum.EAN);
+    assertId(result.getIdentifiers().get(3), localId.getDoc().get(LOCAL_ID_VALUE), TypeEnum.LOCALID);
+    assertId(result.getIdentifiers().get(4), otherId.getDoc().get(NAME), TypeEnum.UNKNOWN);
     assertThat(result.getPublications().get(0).getDateOfPublication()).isEqualTo(
-      publication.getDoc().get(DATE).get(0).textValue());
-    assertThat(result.getPublications().get(0).getPublisher()).isEqualTo(
-      publication.getDoc().get(NAME).get(0).textValue());
-    assertThat(result.getEditionStatement()).isEqualTo(
-      instance.getDoc().get(EDITION_STATEMENT).get(0).textValue());
+      pub.getDoc().get(DATE).get(0).textValue());
+    assertThat(result.getPublications().get(0).getPublisher()).isEqualTo(pub.getDoc().get(NAME).get(0).textValue());
+    assertThat(result.getEditionStatement()).isEqualTo(instance.getDoc().get(EDITION_STATEMENT).get(0).textValue());
+  }
+
+  private Resource getTitle(String type) {
+    var title = new Resource();
+    title.setResourceHash(randomLong());
+    title.setDoc(getJsonNode(Map.of(
+      MAIN_TITLE, List.of(UUID.randomUUID().toString()),
+      SUBTITLE, List.of(UUID.randomUUID().toString())))
+    );
+    title.addType(new ResourceType().setTypeUri(type));
+    return title;
+  }
+
+  private Resource getIdentifier(String valueField, String type) {
+    var id = new Resource();
+    id.setResourceHash(randomLong());
+    id.setDoc(getJsonNode(Map.of(valueField, List.of(randomLong()))));
+    id.addType(new ResourceType().setTypeUri(type));
+    return id;
+  }
+
+  private void assertTitle(BibframeTitlesInner titleInner, JsonNode valueNode, BibframeTitlesInner.TypeEnum type) {
+    assertThat(titleInner.getValue()).isEqualTo(valueNode.get(0).asText());
+    assertThat(titleInner.getType()).isEqualTo(type);
+  }
+
+  private void assertId(BibframeIdentifiersInner idInner, JsonNode valueNode, TypeEnum type) {
+    assertThat(idInner.getValue()).isEqualTo(valueNode.get(0).asText());
+    assertThat(idInner.getType()).isEqualTo(type);
   }
 }
