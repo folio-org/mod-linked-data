366d24d3e7e107f1f9944c852952357ddd37dce5
askhat-abishev <askhat_abishev@epam.com>
2023-11-09 17:27:07 +0600

    MODLD-154: Fix ReIndexControllerFolioIT

--- src/test/java/org/folio/linked/data/e2e/ReIndexControllerFolioIT.java
+++ src/test/java/org/folio/linked/data/e2e/ReIndexControllerFolioIT.java
@@ -5,23 +5,19 @@ import static org.folio.linked.data.test.TestUtil.FOLIO_TEST_PROFILE;
 import static org.folio.linked.data.test.TestUtil.TENANT_ID;
 import static org.folio.linked.data.util.Constants.FOLIO_PROFILE;
 import static org.folio.linked.data.util.Constants.SEARCH_PROFILE;
-import static org.hamcrest.Matchers.containsString;
+import static org.folio.search.domain.dto.ResourceEventType.CREATE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
-import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import org.folio.linked.data.model.entity.Resource;
 import org.folio.linked.data.test.kafka.KafkaSearchIndexTopicListener;
-import org.folio.search.domain.dto.ResourceEventType;
 import org.folio.spring.tools.kafka.KafkaAdminService;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({FOLIO_PROFILE, FOLIO_TEST_PROFILE, SEARCH_PROFILE})
-@Disabled("To be enabled in part 3 task")
 public class ReIndexControllerFolioIT extends ReIndexControllerIT {
 
   @Autowired
@@ -35,11 +31,10 @@ public class ReIndexControllerFolioIT extends ReIndexControllerIT {
   @Override
   @SneakyThrows
   protected void checkKafkaMessageSent(Resource persisted) {
-    boolean messageConsumed = consumer.getLatch().await(30, TimeUnit.SECONDS);
-    assertTrue(messageConsumed);
     if (nonNull(persisted)) {
-      MatcherAssert.assertThat(consumer.getPayload(), containsString(persisted.getResourceHash().toString()));
-      MatcherAssert.assertThat(consumer.getPayload(), containsString(ResourceEventType.CREATE.getValue()));
+      await().untilAsserted(() -> assertTrue(consumer.getMessages()
+        .stream()
+        .anyMatch(m -> m.contains(persisted.getResourceHash().toString()) && m.contains(CREATE.getValue()))));
     }
   }
 }
