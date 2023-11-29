eefa5a7934fb4dce67932bbdb543f8700f87d86e
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2023-11-29 20:42:09 +0600

    MODLD-171: clean and check if publication date is parsed before indexing (#129)

--- src/main/java/org/folio/linked/data/util/BibframeUtils.java
+++ src/main/java/org/folio/linked/data/util/BibframeUtils.java
@@ -4,15 +4,29 @@ import static java.util.Objects.isNull;
 import static java.util.Optional.ofNullable;
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.ld.dictionary.PropertyDictionary;
 
+@Log4j2
 @UtilityClass
 public class BibframeUtils {
+
+  private final String DATE_CLEAN_PATTERN = "[^0-9T:\\-+.]";
+  private final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+    .appendOptional(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+    .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+    .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE)
+    .appendOptional(DateTimeFormatter.ofPattern("yyyy"))
+    .toFormatter();
+
 
   public static String getFirstValue(Supplier<List<String>> valuesSupplier) {
     if (isNull(valuesSupplier)) {
@@ -30,5 +44,21 @@ public class BibframeUtils {
 
   public static void putProperty(Map<String, List<String>> map, PropertyDictionary property, List<String> values) {
     ofNullable(values).filter(v -> !v.isEmpty()).ifPresent(v -> map.put(property.getValue(), v));
+  }
+
+  public static String cleanDate(String initialValue) {
+    return ofNullable(initialValue)
+      .map(iv -> iv.replaceAll(DATE_CLEAN_PATTERN, ""))
+      .filter(StringUtils::isNotEmpty)
+      .map(cleanedValue -> {
+        try {
+          DATE_TIME_FORMATTER.parse(cleanedValue);
+          return cleanedValue;
+        } catch (DateTimeParseException e) {
+          log.warn("Date {} was ignored as invalid", initialValue);
+          return null;
+        }
+      })
+      .orElse(null);
   }
 }
--- src/test/java/org/folio/linked/data/utils/BibframeUtilsTest.java
+++ src/test/java/org/folio/linked/data/utils/BibframeUtilsTest.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import org.folio.linked.data.util.BibframeUtils;
 import org.folio.spring.test.type.UnitTest;
 import org.junit.jupiter.api.Test;
@@ -79,6 +80,38 @@ class BibframeUtilsTest {
 
     // then
     assertThat(result).isEqualTo("");
+  }
+
+  @Test
+  void cleanDate_shouldCleanAnyUnexpectedSymbolAndReturnDateIfParsed() {
+    // given
+    var given = List.of(
+      "2021-11-08T13:00:00.000+00:00",
+      "2022-12-22T13:00:00",
+      "2020-01-01",
+      "[2010]",
+      "sdvsdvindfinvdfnbv±!@#$%^&*()_=2000",
+      "2023",
+      "abc",
+      "25",
+      "200"
+    );
+
+    // when
+    var result = given.stream().map(BibframeUtils::cleanDate).collect(Collectors.toList());
+
+    // then
+    assertThat(result).containsExactly(
+      "2021-11-08T13:00:00.000+00:00",
+      "2022-12-22T13:00:00",
+      "2020-01-01",
+      "2010",
+      "2000",
+      "2023",
+      null,
+      null,
+      null
+    );
   }
 
 }
