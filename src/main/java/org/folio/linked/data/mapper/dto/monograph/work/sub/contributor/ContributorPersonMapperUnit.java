9d3df0649375b896872aa9a0fa7209ffb2ba19b9
askhat-abishev <askhat_abishev@epam.com>
2024-03-20 18:15:11 +0500

    MODLD-274: Update API contract for creator & contributor of Work

---
+++ src/main/java/org/folio/linked/data/mapper/dto/monograph/work/sub/contributor/ContributorPersonMapperUnit.java
@@ -0,0 +1,19 @@
+package org.folio.linked.data.mapper.dto.monograph.work.sub.contributor;
+
+import static org.folio.ld.dictionary.PredicateDictionary.CONTRIBUTOR;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.PERSON;
+
+import org.folio.linked.data.domain.dto.Agent;
+import org.folio.linked.data.mapper.dto.common.MapperUnit;
+import org.folio.linked.data.mapper.dto.monograph.work.sub.AgentRoleAssigner;
+import org.folio.linked.data.repo.ResourceRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@MapperUnit(type = PERSON, dtoClass = Agent.class, predicate = CONTRIBUTOR)
+public class ContributorPersonMapperUnit extends ContributorMapperUnit {
+
+  public ContributorPersonMapperUnit(AgentRoleAssigner agentRoleAssigner, ResourceRepository resourceRepository) {
+    super(agentRoleAssigner, resourceRepository);
+  }
+}
