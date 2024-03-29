551ae9cc613aadb02496fa5a59f9e1191d06b955
askhat-abishev <askhat_abishev@epam.com>
2024-03-29 20:19:16 +0500

    MODLD-286: fix review remarks

--- src/main/java/org/folio/linked/data/service/impl/BatchIndexServiceImpl.java
+++ src/main/java/org/folio/linked/data/service/impl/BatchIndexServiceImpl.java
@@ -4,7 +4,7 @@ import static org.folio.linked.data.util.Constants.SEARCH_PROFILE;
 import static org.folio.search.domain.dto.ResourceEventType.CREATE;
 
 import jakarta.persistence.EntityManager;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,6 +14,7 @@ import org.folio.linked.data.mapper.kafka.KafkaMessageMapper;
 import org.folio.linked.data.model.entity.Resource;
 import org.folio.linked.data.service.BatchIndexService;
 import org.folio.linked.data.service.KafkaSender;
+import org.folio.search.domain.dto.BibframeIndex;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,31 +34,43 @@ public class BatchIndexServiceImpl implements BatchIndexService {
   public BatchIndexResult index(Stream<Resource> resources) {
     var recordsIndexed = new AtomicInteger(0);
     var indexedIds = resources
-      .map(resource -> {
-        try {
-          var id = kafkaMessageMapper.toIndex(resource, CREATE)
-            .map(bibframeIndex -> {
-              log.info("Sending resource for indexing with id {}", bibframeIndex.getId());
-              kafkaSender.sendResourceCreated(bibframeIndex, false);
-              recordsIndexed.getAndIncrement();
-              return resource.getId();
-            })
-            .orElseGet(() -> {
-              log.info("Resource with id {} wasn't sent for indexing, because it doesn't contain any "
-                  + "indexable values. Saving with indexDate to keep it ignored in future indexing",
-                resource.getId());
-              return resource.getId();
-            });
-          // detach the resource entity from entity manager, enabling garbage collection
-          entityManager.detach(resource);
-          return id;
-        } catch (Exception ex) {
-          log.warn("Failed to send resource for indexing with id {}", resource.getId(), ex);
-          return null;
-        }
-      })
-      .filter(Objects::nonNull)
+      .map(resource -> handleResource(resource, recordsIndexed))
+      .filter(Optional::isPresent)
+      .flatMap(Optional::stream)
       .collect(Collectors.toSet());
     return new BatchIndexResult(recordsIndexed.get(), indexedIds);
+  }
+
+  private Optional<Long> handleResource(Resource resource, AtomicInteger recordsIndexed) {
+    try {
+      kafkaMessageMapper.toIndex(resource, CREATE)
+        .ifPresentOrElse(
+          bibframeIndex -> {
+            sendKafkaMessage(bibframeIndex);
+            recordsIndexed.getAndIncrement();
+          },
+          () -> logFailure(resource.getId())
+        );
+      return Optional.of(resource.getId());
+    } catch (Exception ex) {
+      log.warn("Failed to send resource for indexing with id {}", resource.getId(), ex);
+      return Optional.empty();
+    } finally {
+      enableGarbageCollection(resource);
+    }
+  }
+
+  private void sendKafkaMessage(BibframeIndex bibframeIndex) {
+    log.info("Sending resource for indexing with id {}", bibframeIndex.getId());
+    kafkaSender.sendResourceCreated(bibframeIndex, false);
+  }
+
+  private static void logFailure(Long resourceId) {
+    log.info("Resource with id {} wasn't sent for indexing, because it doesn't contain any "
+      + "indexable values. Saving with indexDate to keep it ignored in future indexing", resourceId);
+  }
+
+  private void enableGarbageCollection(Resource resource) {
+    entityManager.detach(resource);
   }
 }
