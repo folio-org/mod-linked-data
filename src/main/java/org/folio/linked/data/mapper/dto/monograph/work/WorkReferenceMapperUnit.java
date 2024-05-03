5da275569cd28a92b40fddd33314aef2d1f8ffee
askhat-abishev <askhat_abishev@epam.com>
2024-05-03 16:57:04 +0500

    MODLD-351: Replace WorkReference API model with Work

--- src/main/java/org/folio/linked/data/mapper/dto/monograph/work/WorkReferenceMapperUnit.java
+++ src/main/java/org/folio/linked/data/mapper/dto/monograph/work/WorkReferenceMapperUnit.java
@@ -1,45 +1,55 @@
 package org.folio.linked.data.mapper.dto.monograph.work;
 
 import static java.util.Objects.nonNull;
+import static java.util.Optional.ofNullable;
 import static org.folio.ld.dictionary.PredicateDictionary.INSTANTIATES;
+import static org.folio.ld.dictionary.PropertyDictionary.BIBLIOGRAPHY_NOTE;
+import static org.folio.ld.dictionary.PropertyDictionary.LANGUAGE_NOTE;
+import static org.folio.ld.dictionary.PropertyDictionary.NOTE;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.WORK;
 
 import java.util.Collections;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.folio.ld.dictionary.PropertyDictionary;
 import org.folio.linked.data.domain.dto.Instance;
-import org.folio.linked.data.domain.dto.WorkReference;
+import org.folio.linked.data.domain.dto.Work;
 import org.folio.linked.data.exception.NotFoundException;
 import org.folio.linked.data.exception.ValidationException;
 import org.folio.linked.data.mapper.dto.common.CoreMapper;
 import org.folio.linked.data.mapper.dto.common.MapperUnit;
 import org.folio.linked.data.mapper.dto.common.SingleResourceMapperUnit;
+import org.folio.linked.data.mapper.dto.monograph.common.NoteMapper;
 import org.folio.linked.data.model.entity.Resource;
 import org.folio.linked.data.repo.ResourceRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@MapperUnit(type = WORK, predicate = INSTANTIATES, dtoClass = WorkReference.class)
+@MapperUnit(type = WORK, predicate = INSTANTIATES, dtoClass = Work.class)
 public class WorkReferenceMapperUnit implements SingleResourceMapperUnit {
 
   private static final Set<Class<?>> SUPPORTED_PARENTS = Collections.singleton(Instance.class);
+  private static final Set<PropertyDictionary> SUPPORTED_NOTES = Set.of(BIBLIOGRAPHY_NOTE, LANGUAGE_NOTE, NOTE);
+
   private final CoreMapper coreMapper;
+  private final NoteMapper noteMapper;
   private final ResourceRepository resourceRepository;
 
   @Override
   public <P> P toDto(Resource source, P parentDto, Resource parentResource) {
     if (parentDto instanceof Instance instance) {
-      var workReference = coreMapper.toDtoWithEdges(source, WorkReference.class, false);
-      workReference.setId(String.valueOf(source.getId()));
-      instance.addWorkReferenceItem(workReference);
+      var work = coreMapper.toDtoWithEdges(source, Work.class, false);
+      work.setId(String.valueOf(source.getId()));
+      ofNullable(source.getDoc()).ifPresent(doc -> work.setNotes(noteMapper.toNotes(doc, SUPPORTED_NOTES)));
+      instance.addWorkReferenceItem(work);
     }
     return parentDto;
   }
 
   @Override
   public Resource toEntity(Object dto, Resource parentEntity) {
-    var work = (WorkReference) dto;
+    var work = (Work) dto;
     if (nonNull(work.getId())) {
       return resourceRepository.findById(Long.parseLong(work.getId()))
         .orElseThrow(() -> new NotFoundException("Work with id [" + work.getId() + "] is not found"));
