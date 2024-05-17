57ad42d9cef770133a388409619681f0c93655ec
PK Jacob <pkjacob@ebsco.com>
2024-05-17 14:42:32 -0400

    MODLD-372: Consume messages from DI_COMPLETED topic

--- src/test/java/org/folio/linked/data/configuration/json/deserialization/event/DataImportEventDeserializerTest.java
+++ src/test/java/org/folio/linked/data/configuration/json/deserialization/event/DataImportEventDeserializerTest.java
@@ -1,10 +1,13 @@
 package org.folio.linked.data.configuration.json.deserialization.event;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.linked.data.test.KafkaEventsTestDataFixture.authorityEvent;
 import static org.folio.linked.data.test.KafkaEventsTestDataFixture.dataImportEvent;
+import static org.folio.linked.data.test.KafkaEventsTestDataFixture.instanceCreatedEvent;
 import static org.folio.linked.data.test.TestUtil.OBJECT_MAPPER;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.util.Map;
 import org.folio.search.domain.dto.DataImportEvent;
 import org.folio.spring.testing.type.UnitTest;
 import org.junit.jupiter.api.Test;
@@ -13,9 +16,9 @@ import org.junit.jupiter.api.Test;
 class DataImportEventDeserializerTest {
 
   @Test
-  void shouldDeserialize() throws IOException {
+  void should_deserialize_instance_created_event() throws IOException {
+    // given
     String eventId = "event_id_01";
-    String eventType = "event_type_01";
     String tenantId = "tenant_01";
     String marc = """
       {
@@ -25,14 +28,63 @@ class DataImportEventDeserializerTest {
           {"906":{"subfields":[{"a":"7"}]}}
       }""";
 
-    String eventJson = dataImportEvent(eventId, tenantId, eventType, marc);
+    String eventJson = instanceCreatedEvent(eventId, tenantId, marc);
 
-    // verify deserializer
+    // when
     DataImportEvent dataImportEvent = OBJECT_MAPPER.readValue(eventJson, DataImportEvent.class);
 
-    assertEquals(eventId, dataImportEvent.getId());
-    assertEquals(eventType, dataImportEvent.getEventType());
-    assertEquals(tenantId, dataImportEvent.getTenant());
-    assertEquals(marc, dataImportEvent.getMarc());
+    // then
+    assertThat(eventId).isEqualTo(dataImportEvent.getId());
+    assertThat(tenantId).isEqualTo(dataImportEvent.getTenant());
+    assertThat(marc).isEqualTo(dataImportEvent.getMarcBib());
+    assertThat(dataImportEvent.getMarcAuthority()).isNull();
+  }
+
+  @Test
+  void should_deserialize_authority_event() throws IOException {
+    // given
+    String eventId = "event_id_01";
+    String tenantId = "tenant_01";
+    String marc = """
+      {
+        "leader":"01767cam a22003977i 4500",
+        "fields":[
+          {"001":"21009122"},
+          {"100":{"subfields":[{"a":"John Doe"}]}}
+      }""";
+
+    String eventJson = authorityEvent(eventId, tenantId, marc);
+
+    // when
+    DataImportEvent dataImportEvent = OBJECT_MAPPER.readValue(eventJson, DataImportEvent.class);
+
+    // then
+    assertThat(eventId).isEqualTo(dataImportEvent.getId());
+    assertThat(tenantId).isEqualTo(dataImportEvent.getTenant());
+    assertThat(marc).isEqualTo(dataImportEvent.getMarcAuthority());
+    assertThat(dataImportEvent.getMarcBib()).isNull();
+  }
+
+
+  @Test
+  void should_not_deserialize_unsupported_event() throws IOException {
+    // given
+    String eventId = "event_id_01";
+
+    String unsupportedEvent = dataImportEvent(eventId, Map.of(
+      "eventType", "DI_COMPLETED",
+      "tenant", "tenant_01",
+      "context", Map.of(
+        "MARC_BIBLIOGRAPHIC", "{}",
+        "CURRENT_EVENT_TYPE", "UNSUPPORTED_EVENT_TYPE"
+      )
+    ));
+
+    // when
+    DataImportEvent dataImportEvent = OBJECT_MAPPER.readValue(unsupportedEvent, DataImportEvent.class);
+
+    // then
+    assertThat(dataImportEvent.getMarcBib()).isNull();
+    assertThat(dataImportEvent.getMarcAuthority()).isNull();
   }
 }
