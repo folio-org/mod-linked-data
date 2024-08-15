6870b290bbf4b061c60a1a041c4122c834692571
Andrei Bordak <Andrei_Bordak@epam.com>
2024-08-15 19:03:04 +0400

    MODLD-457: Change folio_metadata description

--- src/main/resources/changelog/scripts/v-1.0.0/resource_graph/tables/folio_metadata.sql
+++ src/main/resources/changelog/scripts/v-1.0.0/resource_graph/tables/folio_metadata.sql
@@ -7,8 +7,8 @@ create table if not exists folio_metadata (
 
 create index if not exists folio_metadata_inventory_id_idx on folio_metadata(inventory_id);
 
-comment on table folio_metadata is 'Metadata for an instance resource';
+comment on table folio_metadata is 'Stores FOLIO metadata of LD resources';
 comment on column folio_metadata.resource_hash is 'The unique hash identifier for the resource';
-comment on column folio_metadata.inventory_id is 'ID of the instance in FOLIO inventory application';
-comment on column folio_metadata.srs_id is 'ID of the instance in FOLIO SRS application';
-comment on column folio_metadata.source is 'Source of the instance resource';
+comment on column folio_metadata.inventory_id is 'ID of the inventory in FOLIO Inventory application';
+comment on column folio_metadata.srs_id is 'ID of the source record in FOLIO SRS application';
+comment on column folio_metadata.source is 'Source of the instance resource (ex. LINKED_DATA, MARC)';
