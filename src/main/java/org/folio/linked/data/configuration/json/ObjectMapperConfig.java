0d45e54ce2c94dcaaaf22e19ad04cb15ce057fbb
PBobylev <pavel_bobylev@epam.com>
2024-09-23 18:24:51 +0500

    MODSOURCE-802: SourceRecordDomainEventDeserializer introduced

--- src/main/java/org/folio/linked/data/configuration/json/ObjectMapperConfig.java
+++ src/main/java/org/folio/linked/data/configuration/json/ObjectMapperConfig.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.folio.linked.data.configuration.json.deserialization.ResourceRequestFieldDeserializer;
+import org.folio.linked.data.configuration.json.deserialization.event.SourceRecordDomainEventDeserializer;
 import org.folio.linked.data.configuration.json.deserialization.instance.InstanceRequestAllOfMapDeserializer;
 import org.folio.linked.data.configuration.json.deserialization.title.TitleFieldRequestDeserializer;
 import org.folio.linked.data.configuration.json.serialization.MarcRecordSerializationConfig;
 import org.folio.linked.data.domain.dto.InstanceRequestAllOfMap;
 import org.folio.linked.data.domain.dto.MarcRecord;
 import org.folio.linked.data.domain.dto.ResourceRequestField;
+import org.folio.linked.data.domain.dto.SourceRecordDomainEvent;
 import org.folio.linked.data.domain.dto.TitleFieldRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,19 +25,21 @@ public class ObjectMapperConfig {
 
   @Bean
   public ObjectMapper objectMapper() {
-    return new ObjectMapper()
+    var om = new ObjectMapper()
       .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
       .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
       .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
-      .addMixIn(MarcRecord.class, MarcRecordSerializationConfig.class)
-      .registerModule(monographModule());
+      .addMixIn(MarcRecord.class, MarcRecordSerializationConfig.class);
+    om.registerModule(monographModule(om));
+    return om;
   }
 
-  private Module monographModule() {
+  private Module monographModule(ObjectMapper objectMapper) {
     return new SimpleModule()
       .addDeserializer(ResourceRequestField.class, new ResourceRequestFieldDeserializer())
       .addDeserializer(TitleFieldRequest.class, new TitleFieldRequestDeserializer())
-      .addDeserializer(InstanceRequestAllOfMap.class, new InstanceRequestAllOfMapDeserializer());
+      .addDeserializer(InstanceRequestAllOfMap.class, new InstanceRequestAllOfMapDeserializer())
+      .addDeserializer(SourceRecordDomainEvent.class, new SourceRecordDomainEventDeserializer(objectMapper));
   }
 
 }
