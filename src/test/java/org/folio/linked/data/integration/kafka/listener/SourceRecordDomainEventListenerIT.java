cee99967724eb7cc46411dca57b9fcc4b2fa7eaf
PBobylev <pavel_bobylev@epam.com>
2024-09-23 20:04:52 +0500

    MODSOURCE-802: test coverage

--- src/test/java/org/folio/linked/data/integration/kafka/listener/SourceRecordDomainEventListenerIT.java
+++ src/test/java/org/folio/linked/data/integration/kafka/listener/SourceRecordDomainEventListenerIT.java
@@ -7,13 +7,18 @@ import static org.folio.linked.data.test.TestUtil.TENANT_ID;
 import static org.folio.linked.data.test.TestUtil.awaitAndAssert;
 import static org.folio.linked.data.test.kafka.KafkaEventsTestDataFixture.getSrsDomainEvent;
 import static org.folio.linked.data.test.kafka.KafkaEventsTestDataFixture.getSrsDomainEventProducerRecord;
+import static org.folio.linked.data.test.kafka.KafkaEventsTestDataFixture.getSrsDomainEventSampleProducerRecord;
 import static org.folio.linked.data.util.Constants.FOLIO_PROFILE;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.folio.linked.data.domain.dto.ParsedRecord;
+import org.folio.linked.data.domain.dto.SourceRecord;
+import org.folio.linked.data.domain.dto.SourceRecordDomainEvent;
 import org.folio.linked.data.e2e.base.IntegrationTest;
 import org.folio.linked.data.integration.kafka.listener.handler.SourceRecordDomainEventHandler;
+import org.folio.linked.data.test.TestUtil;
 import org.folio.spring.tools.kafka.KafkaAdminService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -40,18 +45,21 @@ class SourceRecordDomainEventListenerIT {
   @Test
   void shouldConsumeSrsDomainEvent() {
     // given
-    var eventId = "event_id_01";
-    var marc = "{}";
-    var recordType = MARC_BIB;
-    var eventType = CREATED;
-    var eventProducerRecord = getSrsDomainEventProducerRecord(eventId, marc, eventType, recordType);
-    var expectedEvent = getSrsDomainEvent(eventId, marc, eventType);
+    var eventProducerRecord = getSrsDomainEventSampleProducerRecord();
+    var expectedEvent = new SourceRecordDomainEvent("23b34a6f-c095-41ea-917b-9765add1a444", CREATED).eventPayload(
+      new SourceRecord()
+        .id("758f57ff-7dad-419e-803c-44b621400c11")
+        .parsedRecord(new ParsedRecord()
+          .content(TestUtil.loadResourceAsString("samples/srsDomainEventParsedContent.txt"))
+        )
+        .deleted(true)
+    );
 
     // when
     eventKafkaTemplate.send(eventProducerRecord);
 
     // then
-    awaitAndAssert(() -> verify(sourceRecordDomainEventHandler).handle(expectedEvent, recordType));
+    awaitAndAssert(() -> verify(sourceRecordDomainEventHandler).handle(expectedEvent, MARC_BIB));
   }
 
   @Test
