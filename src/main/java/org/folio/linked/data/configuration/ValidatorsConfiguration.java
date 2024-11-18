4433d1e74c8f21730f26e867647c28834fb6d9d9
askhat-abishev <askhat_abishev@epam.com>
2024-11-18 12:30:10 +0500

    MODLD-583: fix

--- src/main/java/org/folio/linked/data/configuration/ValidatorsConfiguration.java
+++ src/main/java/org/folio/linked/data/configuration/ValidatorsConfiguration.java
@@ -4,6 +4,8 @@ import static org.folio.linked.data.util.Constants.Validators.VALIDATOR_CHAIN_LD
 
 import org.folio.linked.data.domain.dto.ResourceRequestDto;
 import org.folio.linked.data.service.validation.LdValidator;
+import org.folio.linked.data.service.validation.validators.LccnPatternValidator;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,9 @@ import org.springframework.context.annotation.Configuration;
 public class ValidatorsConfiguration {
 
   @Bean(VALIDATOR_CHAIN_LD)
-  public LdValidator<ResourceRequestDto> resourceSaveValidatorsChain(LdValidator<ResourceRequestDto> lccnValidator) {
+  public LdValidator<ResourceRequestDto> resourceSaveValidatorsChain(
+    @Qualifier(LccnPatternValidator.CODE) LdValidator<ResourceRequestDto> lccnPatternValidator) {
     // in future task we can filter out disabled validators here
-    return lccnValidator;
+    return lccnPatternValidator;
   }
 }
