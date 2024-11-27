3e6382e1450da490c840015b2d6cd1bb1c6010e8
askhat-abishev <askhat_abishev@epam.com>
2024-11-27 17:41:41 +0500

    MODLD-585: fix AuthorityAssignmentController interface

--- src/main/java/org/folio/linked/data/controller/AuthorityAssignmentController.java
+++ src/main/java/org/folio/linked/data/controller/AuthorityAssignmentController.java
@@ -4,7 +4,7 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import lombok.RequiredArgsConstructor;
 import org.folio.linked.data.domain.dto.AssignmentCheckDto;
-import org.folio.linked.data.rest.resource.LinkedDataApi;
+import org.folio.linked.data.rest.resource.AuthorityApi;
 import org.folio.linked.data.service.resource.AssignAuthorityTarget;
 import org.folio.linked.data.service.resource.ResourceMarcAuthorityService;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class AuthorityAssignmentController implements LinkedDataApi {
+public class AuthorityAssignmentController implements AuthorityApi {
 
   private final ResourceMarcAuthorityService resourceMarcAuthorityService;
 
