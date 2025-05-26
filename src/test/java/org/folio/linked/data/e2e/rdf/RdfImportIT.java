86ca262ea0716bf614302a5d4dae19423de93d8e
PBobylev <pavel_bobylev@epam.com>
2025-05-26 14:45:46 +0500

    MODLD-741: make RdfImportService transactional

--- src/test/java/org/folio/linked/data/e2e/rdf/RdfImportIT.java
+++ src/test/java/org/folio/linked/data/e2e/rdf/RdfImportIT.java
@@ -2,14 +2,18 @@ package org.folio.linked.data.e2e.rdf;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.folio.linked.data.test.TestUtil.defaultHeaders;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.folio.linked.data.e2e.base.IntegrationTest;
+import org.folio.linked.data.integration.ResourceModificationEventListener;
 import org.folio.linked.data.repo.ResourceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,6 +26,8 @@ class RdfImportIT {
   private Environment env;
   @Autowired
   private ResourceRepository resourceRepo;
+  @MockitoSpyBean
+  private ResourceModificationEventListener eventListener;
 
   @Test
   void rdfImport_shouldSaveImportedResourceAndSendEventAndReturnId() throws Exception {
@@ -43,6 +49,7 @@ class RdfImportIT {
       .andReturn().getResponse().getContentAsString();
     assertThat(response).isEqualTo("[" + expectedId + "]");
     assertThat(resourceRepo.existsById(expectedId)).isTrue();
+    verify(eventListener).afterCreate(any());
   }
 
 }
