fb2c6f1a052f95d8dd27d9245ae405c030ab6bb1
PK Jacob <pkjacob@ebsco.com>
2025-05-28 12:38:24 -0400

    MODLD-744: API to return profile metadata array for a resource type

--- src/main/java/org/folio/linked/data/service/ProfileService.java
+++ src/main/java/org/folio/linked/data/service/ProfileService.java
@@ -1,8 +1,16 @@
 package org.folio.linked.data.service;
 
+import java.util.List;
+import org.folio.linked.data.domain.dto.ProfileMetadata;
+import org.folio.linked.data.model.entity.Profile;
+
 public interface ProfileService {
+
+  Profile saveProfile(Long id, String name, String resourceTypeUri, String value);
 
   String getProfile();
 
-  String getProfileById(String id);
+  String getProfileById(Long id);
+
+  List<ProfileMetadata> getMetadataByResourceType(String resourceTypeUri);
 }
