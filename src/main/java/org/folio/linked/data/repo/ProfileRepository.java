eeff2a6be1f5d69f3ffb015d6cef460ea2856bd5
PK Jacob <pkjacob@ebsco.com>
2025-05-30 09:14:28 -0400

    MODLD-744: update uri to /profile; Removed hardcoded SQL from ProfileRepository

--- src/main/java/org/folio/linked/data/repo/ProfileRepository.java
+++ src/main/java/org/folio/linked/data/repo/ProfileRepository.java
@@ -2,11 +2,8 @@ package org.folio.linked.data.repo;
 
 import java.util.List;
 import org.folio.linked.data.model.entity.Profile;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
-  @Query("SELECT p FROM Profile p WHERE p.resourceType.uri = :resourceTypeUri")
-  List<Profile> findByResourceTypeUri(@Param("resourceTypeUri") String resourceTypeUri);
+  List<Profile> findByResourceTypeUri(String resourceTypeUri);
 }
