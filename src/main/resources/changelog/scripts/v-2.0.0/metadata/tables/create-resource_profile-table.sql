a897628aa301132ee33cdcd03d55d6d3e507e783
PK Jacob <pkjacob@ebsco.com>
2025-06-24 14:34:54 -0400

    MODLD-746: Implementing partitioning of resource_profile table

--- src/main/resources/changelog/scripts/v-2.0.0/metadata/tables/create-resource_profile-table.sql
+++ src/main/resources/changelog/scripts/v-2.0.0/metadata/tables/create-resource_profile-table.sql
@@ -11,6 +11,11 @@ create table if not exists resource_profile
   foreign key (profile_id) references profiles (id) on delete cascade
 ) partition by hash(resource_hash);
 
+create table if not exists resource_profile_4_0 partition of resource_profile for values with (modulus 4, remainder 0);
+create table if not exists resource_profile_4_1 partition of resource_profile for values with (modulus 4, remainder 1);
+create table if not exists resource_profile_4_2 partition of resource_profile for values with (modulus 4, remainder 2);
+create table if not exists resource_profile_4_3 partition of resource_profile for values with (modulus 4, remainder 3);
+
 comment on table resource_profile is 'Associate resource with a profile';
 comment on column resource_profile.resource_hash is 'Identifier for the resource (resource_hash from resources table)';
 comment on column resource_profile.profile_id is 'Reference to the associated profile';
