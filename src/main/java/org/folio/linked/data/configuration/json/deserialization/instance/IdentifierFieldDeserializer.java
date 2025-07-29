23b1a8cbe5997f8668de9ba6dea218eed90dc40e
PK Jacob <pkjacob@ebsco.com>
2025-07-29 16:07:12 -0400

    MODLD-799: Update API to support ISSN

--- src/main/java/org/folio/linked/data/configuration/json/deserialization/instance/IdentifierFieldDeserializer.java
+++ src/main/java/org/folio/linked/data/configuration/json/deserialization/instance/IdentifierFieldDeserializer.java
@@ -2,6 +2,7 @@ package org.folio.linked.data.configuration.json.deserialization.instance;
 
 import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_EAN;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_ISBN;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_ISSN;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_LCCN;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_LOCAL;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.ID_UNKNOWN;
@@ -14,6 +15,7 @@ import java.util.Map;
 import org.folio.linked.data.domain.dto.EanField;
 import org.folio.linked.data.domain.dto.IdentifierField;
 import org.folio.linked.data.domain.dto.IsbnField;
+import org.folio.linked.data.domain.dto.IssnField;
 import org.folio.linked.data.domain.dto.LccnField;
 import org.folio.linked.data.domain.dto.LocalIdField;
 import org.folio.linked.data.domain.dto.OtherIdField;
@@ -24,6 +26,7 @@ public class IdentifierFieldDeserializer extends JsonDeserializer<IdentifierFiel
 
   private static final Map<String, Class<? extends IdentifierField>> IDENTITY_MAP = Map.of(
     ID_LCCN.getUri(), LccnField.class,
+    ID_ISSN.getUri(), IssnField.class,
     ID_ISBN.getUri(), IsbnField.class,
     ID_EAN.getUri(), EanField.class,
     ID_LOCAL.getUri(), LocalIdField.class,
