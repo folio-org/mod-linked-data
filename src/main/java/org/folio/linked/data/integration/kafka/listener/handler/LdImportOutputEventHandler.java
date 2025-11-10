bdb77eaec462102c00c04401d5312b68c1463cbf
PBobylev <pavel_bobylev@epam.com>
2025-11-10 15:08:05 +0500

    MODLDI-7: RdfImport: handle resource saving exception

--- src/main/java/org/folio/linked/data/integration/kafka/listener/handler/LdImportOutputEventHandler.java
+++ src/main/java/org/folio/linked/data/integration/kafka/listener/handler/LdImportOutputEventHandler.java
@@ -2,6 +2,7 @@ package org.folio.linked.data.integration.kafka.listener.handler;
 
 import static org.folio.linked.data.util.Constants.STANDALONE_PROFILE;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.linked.data.domain.dto.ImportOutputEvent;
@@ -18,7 +19,21 @@ public class LdImportOutputEventHandler implements ExternalEventHandler<ImportOu
 
   public void handle(ImportOutputEvent event) {
     log.info("Handling LD Import output event with id {} for tenant {}", event.getTs(), event.getTenant());
-    event.getResources().forEach(resourceService::saveResource);
+    var counter = new AtomicInteger();
+    event.getResources().forEach(resource -> {
+      try {
+        resourceService.saveResource(resource);
+        counter.getAndIncrement();
+      } catch (Exception e) {
+        if (log.isDebugEnabled()) {
+          log.debug("Exception during saving LDImport resource: {}", resource);
+        } else {
+          log.error("Exception during saving LDImport resource with id [{}]", resource.getId());
+        }
+      }
+    });
+    log.info("{} of {} resource(s) saved out of LDImportOutput event with ID {} for tenant {}", counter.get(),
+      event.getResources().size(), event.getTs(), event.getTenant());
   }
 
 }
