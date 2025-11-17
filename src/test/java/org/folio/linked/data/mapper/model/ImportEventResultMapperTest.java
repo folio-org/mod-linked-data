444702be09a3ac8202f337337640af742c4b91f8
PBobylev <pavel_bobylev@epam.com>
2025-11-17 19:34:09 +0500

    MODLD-911: fixed test inclusion

--- src/test/java/org/folio/linked/data/mapper/model/ImportEventResultMapperTest.java
+++ src/test/java/org/folio/linked/data/mapper/model/ImportEventResultMapperTest.java
@@ -12,17 +12,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.linked.data.domain.dto.ImportOutputEvent;
-import org.folio.linked.data.model.entity.imprt.ImportEventResult;
 import org.folio.linked.data.util.ImportUtils;
+import org.folio.spring.testing.type.UnitTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@UnitTest
 @ExtendWith(MockitoExtension.class)
 class ImportEventResultMapperTest {
 
@@ -113,55 +113,6 @@ class ImportEventResultMapperTest {
     assertThat(importEventFailedResource.getImportEventResult()).isEqualTo(result);
     assertThat(importEventFailedResource.getRawResource())
       .isEqualTo("mapping to json failed: " + expectedJsonProcessingException);
-    assertThat(importEventFailedResource.getReason()).isEqualTo(failureReason);
-  }
-
-  @Test
-  void getFailedResources_shouldMapCorrectData() throws JsonProcessingException {
-    // given
-    var failureReason = "failure reason";
-    var imports = List.of(
-      getImportedResource(1L, "created", CREATED, null),
-      getImportedResource(2L, "updated", UPDATED, null),
-      getImportedResource(3L, "failed", FAILED, failureReason)
-    );
-    var expectedRawFailedResource = "expectedRawFailedResource";
-    when(objectMapper.writeValueAsString(any())).thenReturn(expectedRawFailedResource);
-    var ier = new ImportEventResult();
-
-    // when
-    var result = mapper.getFailedResources(ier, imports);
-
-    // then
-    assertThat(result).hasSize(1);
-    var importEventFailedResource = result.iterator().next();
-    assertThat(importEventFailedResource.getImportEventResult()).isEqualTo(ier);
-    assertThat(importEventFailedResource.getRawResource()).isEqualTo(expectedRawFailedResource);
-    assertThat(importEventFailedResource.getReason()).isEqualTo(failureReason);
-  }
-
-  @Test
-  void getFailedResources_shouldMap_whenFailedResourceNotCorrectJson() throws JsonProcessingException {
-    // given
-    var failureReason = "failure reason";
-    var imports = List.of(
-      getImportedResource(1L, "created", CREATED, null),
-      getImportedResource(2L, "updated", UPDATED, null),
-      getImportedResource(3L, "failed", FAILED, failureReason)
-    );
-    var expectedException = "expectedException";
-    when(objectMapper.writeValueAsString(any())).thenThrow(new JsonParseException("expectedException"));
-    var ier = new ImportEventResult();
-
-    // when
-    var result = mapper.getFailedResources(ier, imports);
-
-    // then
-    assertThat(result).hasSize(1);
-    var importEventFailedResource = result.iterator().next();
-    assertThat(importEventFailedResource.getImportEventResult()).isEqualTo(ier);
-    assertThat(importEventFailedResource.getRawResource()).isEqualTo("mapping to json failed: "
-      + expectedException);
     assertThat(importEventFailedResource.getReason()).isEqualTo(failureReason);
   }
 
