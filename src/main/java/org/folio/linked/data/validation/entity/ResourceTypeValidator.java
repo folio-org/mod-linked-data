69d9f0da83872e60879985524b807154525c67ae
PBobylev <pavel_bobylev@epam.com>
2025-12-19 18:56:01 +0500

    MODLD-789: MOCKED_RESOURCE type handling and validation

--- src/main/java/org/folio/linked/data/validation/entity/ResourceTypeValidator.java
+++ src/main/java/org/folio/linked/data/validation/entity/ResourceTypeValidator.java
@@ -1,6 +1,7 @@
 package org.folio.linked.data.validation.entity;
 
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.MOCKED_RESOURCE;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -10,6 +11,6 @@ import org.folio.linked.data.validation.ResourceTypeConstraint;
 public class ResourceTypeValidator implements ConstraintValidator<ResourceTypeConstraint, Resource> {
   @Override
   public boolean isValid(Resource resource, ConstraintValidatorContext constraintValidatorContext) {
-    return isNotEmpty(resource.getTypes());
+    return isNotEmpty(resource.getTypes()) && resource.isNotOfType(MOCKED_RESOURCE);
   }
 }
