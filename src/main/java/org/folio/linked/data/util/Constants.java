1a1c9df4a8ad1281ce0dc23a3e6cd62463b8c3d6
PK Jacob <pkjacob@ebsco.com>
2026-01-08 14:16:16 -0500

    MODLD-923: Set LINK of the identity resource based on FOLIO configuration

--- src/main/java/org/folio/linked/data/util/Constants.java
+++ src/main/java/org/folio/linked/data/util/Constants.java
@@ -29,5 +29,6 @@ public class Constants {
     public static final String SETTINGS_ENTRIES = "settings-entries";
     public static final String MODULE_STATE = "module-state";
     public static final String PROFILES = "profiles";
+    public static final String AUTHORITY_SOURCE_FILES = "authority-source-files";
   }
 }
