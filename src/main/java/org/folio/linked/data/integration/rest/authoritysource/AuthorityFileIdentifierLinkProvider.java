1f3c09a65b1f68d9caa4025c89868e32eb0d0382
PK Jacob <pkjacob@ebsco.com>
2026-01-12 14:22:20 -0500

    MODLD-923: Refactor to use IdentifierPrefixService

--- src/main/java/org/folio/linked/data/integration/rest/authoritysource/AuthorityFileIdentifierLinkProvider.java
+++ src/main/java/org/folio/linked/data/integration/rest/authoritysource/AuthorityFileIdentifierLinkProvider.java
@@ -6,27 +6,38 @@ import static org.folio.linked.data.util.Constants.STANDALONE_PROFILE;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.folio.linked.data.domain.dto.AuthoritySourceFile;
-import org.folio.marc4ld.service.marc2ld.authority.identifier.IdentifierUrlProvider;
+import org.folio.marc4ld.service.marc2ld.authority.identifier.IdentifierLinkProvider;
+import org.folio.marc4ld.service.marc2ld.authority.identifier.IdentifierPrefixService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("identifierUrlProvider")
+@Service("identifierLinkProvider")
 @RequiredArgsConstructor
 @Profile("!" + STANDALONE_PROFILE)
-public class AuthorityFileIdentifierUrlProvider implements IdentifierUrlProvider {
+public class AuthorityFileIdentifierLinkProvider implements IdentifierLinkProvider {
   private final AuthoritySourceFilesClient authoritySourceFilesClient;
+  private final IdentifierPrefixService identifierPrefixService;
   @Value("${mod-linked-data.authority-source-files.limit}")
   private int authoritySourceFilesLimit;
 
   @Override
-  public Optional<String> getBaseUrl(String identifierPrefix) {
+  public Optional<String> getIdentifierLink(String identifier) {
+    var prefix = identifierPrefixService.getIdentifierPrefix(identifier);
     var authoritySourceFiles = authoritySourceFilesClient.getAuthoritySourceFiles(authoritySourceFilesLimit);
 
     return authoritySourceFiles.getAuthoritySourceFiles().stream()
       .filter(source -> nonNull(source.getCodes()))
-      .filter(source -> source.getCodes().stream().anyMatch(identifierPrefix::equals))
+      .filter(source -> source.getCodes().stream().anyMatch(prefix::equals))
       .map(AuthoritySourceFile::getBaseUrl)
+      .map(baseUrl -> this.createLink(baseUrl, identifier))
       .findFirst();
+  }
+
+  private String createLink(String baseUrl, String identifier) {
+    if (!baseUrl.endsWith("/")) {
+      baseUrl = baseUrl + "/";
+    }
+    return baseUrl + identifier;
   }
 }
--- src/test/java/org/folio/linked/data/integration/rest/authoritysource/AuthorityFileIdentifierLinkProviderTest.java
+++ src/test/java/org/folio/linked/data/integration/rest/authoritysource/AuthorityFileIdentifierLinkProviderTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.util.ReflectionTestUtils.setField;
 import java.util.List;
 import org.folio.linked.data.domain.dto.AuthoritySourceFile;
 import org.folio.linked.data.domain.dto.AuthoritySourceFiles;
+import org.folio.marc4ld.service.marc2ld.authority.identifier.IdentifierPrefixService;
 import org.folio.spring.testing.type.UnitTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,13 +18,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @UnitTest
 @ExtendWith(MockitoExtension.class)
-class AuthorityFileIdentifierUrlProviderTest {
+class AuthorityFileIdentifierLinkProviderTest {
 
   @Mock
   private AuthoritySourceFilesClient authoritySourceFilesClient;
-
+  @Mock
+  private IdentifierPrefixService identifierPrefixService;
   @InjectMocks
-  private AuthorityFileIdentifierUrlProvider provider;
+  private AuthorityFileIdentifierLinkProvider provider;
 
   @BeforeEach
   void setUp() {
@@ -33,27 +35,31 @@ class AuthorityFileIdentifierUrlProviderTest {
   @Test
   void getBaseUrl_shouldReturnBaseUrl_whenCodeMatches() {
     // given
+    var identifier = "abc123456";
     var file1 = new AuthoritySourceFile().codes(List.of("abc", "def")).baseUrl("http://example.com/abc");
     var file2 = new AuthoritySourceFile().codes(List.of("xyz")).baseUrl("http://example.com/xyz");
     var files = new AuthoritySourceFiles(List.of(file1, file2));
     when(authoritySourceFilesClient.getAuthoritySourceFiles(50)).thenReturn(files);
+    when(identifierPrefixService.getIdentifierPrefix(identifier)).thenReturn("abc");
 
     // when
-    var result = provider.getBaseUrl("abc");
+    var result = provider.getIdentifierLink(identifier);
 
     // then
-    assertThat(result).contains("http://example.com/abc");
+    assertThat(result).contains("http://example.com/abc/abc123456");
   }
 
   @Test
   void getBaseUrl_shouldReturnEmpty_whenNoCodeMatches() {
     // given
+    var identifier = "nf1234567";
     var file1 = new AuthoritySourceFile().codes(List.of("abc", "def")).baseUrl("http://example.com/abc");
     var files = new AuthoritySourceFiles(List.of(file1));
     when(authoritySourceFilesClient.getAuthoritySourceFiles(50)).thenReturn(files);
+    when(identifierPrefixService.getIdentifierPrefix(identifier)).thenReturn("nf");
 
     // when
-    var result = provider.getBaseUrl("notfound");
+    var result = provider.getIdentifierLink(identifier);
 
     // then
     assertThat(result).isEmpty();
@@ -62,15 +68,17 @@ class AuthorityFileIdentifierUrlProviderTest {
   @Test
   void getBaseUrl_shouldSkipNullCodes() {
     // given
+    var identifier = "xyz123456";
     var file1 = new AuthoritySourceFile().codes(null).baseUrl("http://example.com/abc");
-    var file2 = new AuthoritySourceFile().codes(List.of("xyz")).baseUrl("http://example.com/xyz");
+    var file2 = new AuthoritySourceFile().codes(List.of("xyz")).baseUrl("http://example.com/xyz/");
     var files = new AuthoritySourceFiles(List.of(file1, file2));
     when(authoritySourceFilesClient.getAuthoritySourceFiles(50)).thenReturn(files);
+    when(identifierPrefixService.getIdentifierPrefix(identifier)).thenReturn("xyz");
 
     // when
-    var result = provider.getBaseUrl("xyz");
+    var result = provider.getIdentifierLink(identifier);
 
     // then
-    assertThat(result).contains("http://example.com/xyz");
+    assertThat(result).contains("http://example.com/xyz/xyz123456");
   }
 }
