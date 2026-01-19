fa291f00ada38e667040b0e2a89d0d9cdc9eb19a
pkjacob <121239864+pkjacob@users.noreply.github.com>
2026-01-19 08:16:18 -0500

    MODLD-960: Insert Hub profile to DB; Update ResourceProfileLinkingServiceImpl to support Hub profile (#415)

--- src/test/java/org/folio/linked/data/service/profile/ResourceProfileLinkingServiceImplTest.java
+++ src/test/java/org/folio/linked/data/service/profile/ResourceProfileLinkingServiceImplTest.java
@@ -1,36 +1,23 @@
 package org.folio.linked.data.service.profile;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.folio.ld.dictionary.PredicateDictionary.BOOK_FORMAT;
-import static org.folio.ld.dictionary.PredicateDictionary.INSTANTIATES;
-import static org.folio.ld.dictionary.ResourceTypeDictionary.BOOKS;
-import static org.folio.ld.dictionary.ResourceTypeDictionary.CONTINUING_RESOURCES;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.INSTANCE;
-import static org.folio.ld.dictionary.ResourceTypeDictionary.WORK;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
-import org.folio.ld.dictionary.ResourceTypeDictionary;
-import org.folio.linked.data.domain.dto.ProfileMetadata;
 import org.folio.linked.data.model.entity.Resource;
-import org.folio.linked.data.model.entity.ResourceEdge;
 import org.folio.linked.data.model.entity.ResourceProfile;
 import org.folio.linked.data.model.entity.ResourceTypeEntity;
 import org.folio.linked.data.repo.ResourceProfileRepository;
+import org.folio.linked.data.service.profile.strategy.ProfileSelectionStrategy;
 import org.folio.spring.testing.type.UnitTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -41,11 +28,13 @@ class ResourceProfileLinkingServiceImplTest {
   @Mock
   private ResourceProfileRepository resourceProfileRepository;
 
-  @Mock
-  private PreferredProfileService preferredProfileService;
+  private ResourceProfileLinkingServiceImpl profileLinkingService;
 
-  @InjectMocks
-  private ResourceProfileLinkingServiceImpl resourceProfileLinkingService;
+  @BeforeEach
+  void setUp() {
+    var strategy = new TestStrategy();
+    profileLinkingService = new ResourceProfileLinkingServiceImpl(resourceProfileRepository, null, List.of(strategy));
+  }
 
   @Test
   void shouldLinkResourceToProfile() {
@@ -56,7 +45,7 @@ class ResourceProfileLinkingServiceImplTest {
 
 
     // when
-    resourceProfileLinkingService.linkResourceToProfile(resource, profileId);
+    profileLinkingService.linkResourceToProfile(resource, profileId);
 
     // then
     verify(resourceProfileRepository).save(captor.capture());
@@ -74,67 +63,37 @@ class ResourceProfileLinkingServiceImplTest {
       .thenReturn(Optional.of(() -> profileId));
 
     // when
-    var result = resourceProfileLinkingService.resolveProfileId(resource);
+    var result = profileLinkingService.resolveProfileId(resource);
 
     // then
     assertThat(result).isEqualTo(profileId);
   }
 
   @Test
-  void shouldResolveProfileId_returnUserPreferredProfile() {
+  void shouldResolveProfileIdViaStrategy() {
     // given
-    var profileId = 2;
-    var resource = new Resource()
-      .setTypes(Set.of(new ResourceTypeEntity().setUri(INSTANCE.getUri())))
-      .setIdAndRefreshEdges(1L);
+    var resource = new Resource().setTypes(Set.of(new ResourceTypeEntity().setUri(INSTANCE.getUri())));
     when(resourceProfileRepository.findProfileIdByResourceHash(resource.getId())).thenReturn(Optional.empty());
-    when(preferredProfileService.getPreferredProfiles(INSTANCE.getUri()))
-      .thenReturn(List.of(new ProfileMetadata(profileId, "", "")));
 
     // when
-    var result = resourceProfileLinkingService.resolveProfileId(resource);
+    var result = profileLinkingService.resolveProfileId(resource);
 
     // then
-    assertThat(result).isEqualTo(profileId);
+    assertThat(result).isEqualTo(TestStrategy.TEST_PROFILE_ID);
   }
 
-  @ParameterizedTest
-  @MethodSource("dataProvider")
-  void shouldResolveProfileId_returnDefaultProfileId(int expectedProfileId, Resource resource) {
-    // given
-    when(resourceProfileRepository.findProfileIdByResourceHash(resource.getId())).thenReturn(Optional.empty());
-    lenient().when(preferredProfileService.getPreferredProfiles(any())).thenReturn(List.of());
 
-    // when
-    var result = resourceProfileLinkingService.resolveProfileId(resource);
+  private static final class TestStrategy implements ProfileSelectionStrategy {
+    static final Integer TEST_PROFILE_ID = 99;
 
-    // then
-    assertThat(result).isEqualTo(expectedProfileId);
-  }
+    @Override
+    public boolean supports(Resource resource) {
+      return true;
+    }
 
-  private static Stream<Arguments> dataProvider() {
-    return Stream.of(
-      Arguments.of(2, getWork(BOOKS)),
-      Arguments.of(3, getInstance()),
-      Arguments.of(4, getInstance().addOutgoingEdge(getBookFormatEdge())),
-      Arguments.of(5, getInstance().addOutgoingEdge(getSerialWorkEdge())),
-      Arguments.of(6, getWork(CONTINUING_RESOURCES))
-    );
-  }
-
-  private static Resource getWork(ResourceTypeDictionary extraType) {
-    return new Resource().setIdAndRefreshEdges(1L).addTypes(WORK, extraType);
-  }
-
-  private static Resource getInstance() {
-    return new Resource().setIdAndRefreshEdges(2L).addTypes(INSTANCE);
-  }
-
-  private static ResourceEdge getBookFormatEdge() {
-    return new ResourceEdge(getInstance(), new Resource().setIdAndRefreshEdges(3L), BOOK_FORMAT);
-  }
-
-  private static ResourceEdge getSerialWorkEdge() {
-    return new ResourceEdge(getInstance(), getWork(CONTINUING_RESOURCES), INSTANTIATES);
+    @Override
+    public Integer selectProfile(Resource resource) {
+      return TEST_PROFILE_ID;
+    }
   }
 }
