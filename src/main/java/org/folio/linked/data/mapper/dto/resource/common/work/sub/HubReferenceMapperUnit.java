29a05b78c8d514614117653afe09a5959bddb65e
PK Jacob <pkjacob@ebsco.com>
2026-01-30 16:05:41 -0500

    MODLD-967: HubReferenceMapperUnit extends from ReferenceMapperUnit

--- src/main/java/org/folio/linked/data/mapper/dto/resource/common/work/sub/HubReferenceMapperUnit.java
+++ src/main/java/org/folio/linked/data/mapper/dto/resource/common/work/sub/HubReferenceMapperUnit.java
@@ -5,48 +5,49 @@ import static org.folio.ld.dictionary.PredicateDictionary.RELATED_TO;
 import static org.folio.ld.dictionary.PropertyDictionary.LABEL;
 import static org.folio.ld.dictionary.PropertyDictionary.LINK;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.HUB;
+import static org.folio.linked.data.util.ResourceUtils.getPropertyValues;
 import static org.folio.linked.data.util.ResourceUtils.putProperty;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.HashMap;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.folio.ld.dictionary.PropertyDictionary;
 import org.folio.ld.dictionary.ResourceTypeDictionary;
 import org.folio.linked.data.domain.dto.HubReferenceWithType;
 import org.folio.linked.data.domain.dto.Reference;
 import org.folio.linked.data.domain.dto.WorkResponse;
 import org.folio.linked.data.mapper.dto.resource.base.CoreMapper;
 import org.folio.linked.data.mapper.dto.resource.base.MapperUnit;
+import org.folio.linked.data.mapper.dto.resource.common.work.sub.reference.ReferenceMapperUnit;
 import org.folio.linked.data.model.entity.Resource;
 import org.folio.linked.data.service.reference.ReferenceService;
 import org.folio.linked.data.service.resource.hash.HashService;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 @MapperUnit(type = HUB, predicate = {EXPRESSION_OF, RELATED_TO}, requestDto = Reference.class)
-public class HubReferenceMapperUnit implements WorkSubResourceMapperUnit {
+public class HubReferenceMapperUnit extends ReferenceMapperUnit {
   private final CoreMapper coreMapper;
   private final HashService hashService;
   private final ReferenceService referenceService;
 
+  public HubReferenceMapperUnit(ReferenceService referenceService, HashService hashService, CoreMapper coreMapper) {
+    super(referenceService);
+    this.hashService = hashService;
+    this.coreMapper = coreMapper;
+    this.referenceService = referenceService;
+  }
+
   @Override
   public <P> P toDto(Resource resourceToConvert, P parentDto, ResourceMappingContext context) {
-    if (parentDto instanceof WorkResponse workDto && resourceToConvert.getDoc() != null) {
-      var doc = resourceToConvert.getDoc();
-      var hub = new Reference()
-        .label(doc.get(PropertyDictionary.LABEL.getValue()).get(0).asText())
-        .id(String.valueOf(resourceToConvert.getId()));
-      var linkNode = doc.get(PropertyDictionary.LINK.getValue());
-      if (linkNode != null && !linkNode.isEmpty()) {
-        hub.rdfLink(linkNode.get(0).asText());
-      }
-      workDto.addHubsItem(
-        new HubReferenceWithType()
-          .hub(hub)
-          .relation(context.predicate().getUri())
-      );
+    if (parentDto instanceof WorkResponse workDto) {
+      var reference = toReference(resourceToConvert);
+      getPropertyValues(resourceToConvert, LINK).stream()
+        .findFirst()
+        .ifPresent(reference::setRdfLink);
+      var referenceWithType = new HubReferenceWithType()
+        .hub(reference)
+        .relation(HUB.getUri());
+      workDto.addHubsItem(referenceWithType);
     }
     return parentDto;
   }
