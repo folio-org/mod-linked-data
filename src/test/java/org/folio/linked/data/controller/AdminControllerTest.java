a24e38ee1d85af52cd4993481cd5d4b220794afd
PK Jacob <pkjacob@ebsco.com>
2026-02-20 12:54:12 -0500

    MODLD-995: Addressed comments from Copilot

--- src/test/java/org/folio/linked/data/controller/AdminControllerTest.java
+++ src/test/java/org/folio/linked/data/controller/AdminControllerTest.java
@@ -1,7 +1,8 @@
 package org.folio.linked.data.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.folio.linked.data.rest.resource.AdminApi.PATH_CLEAR_CACHES;
+import static org.folio.linked.data.util.Constants.Cache.PROFILES;
+import static org.folio.linked.data.util.Constants.Cache.SPEC_RULES;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,25 +18,25 @@ class AdminControllerTest {
   void clearCaches_shouldClearAllCachesAndReturnNoContent() throws Exception {
     // given
     var cacheManager = new CaffeineCacheManager();
-    var ruleCacheKey = "tenant1_rule";
-    var profileCacheKey = "tenant1_profile";
 
     var specRulesCache = cacheManager.getCache("spec-rules");
-    specRulesCache.put(ruleCacheKey, "rule-value");
-    assertThat(specRulesCache.get(ruleCacheKey)).isNotNull();
+    assertThat(specRulesCache).isNotNull();
+    specRulesCache.put(SPEC_RULES, "rule-value");
+    assertThat(specRulesCache.get(SPEC_RULES)).isNotNull();
 
     var profilesCache = cacheManager.getCache("profiles");
-    profilesCache.put(profileCacheKey, "profile-value");
-    assertThat(profilesCache.get(profileCacheKey)).isNotNull();
+    assertThat(profilesCache).isNotNull();
+    profilesCache.put(PROFILES, "profile-value");
+    assertThat(profilesCache.get(PROFILES)).isNotNull();
 
     var mockMvc = MockMvcBuilders.standaloneSetup(new AdminController(cacheManager)).build();
 
     // when
-    mockMvc.perform(delete(PATH_CLEAR_CACHES))
+    mockMvc.perform(delete("/linked-data/admin/caches"))
       .andExpect(status().isNoContent());
 
     // then
-    assertThat(specRulesCache.get(ruleCacheKey)).isNull();
-    assertThat(profilesCache.get(profileCacheKey)).isNull();
+    assertThat(specRulesCache.get(SPEC_RULES)).isNull();
+    assertThat(profilesCache.get(PROFILES)).isNull();
   }
 }
