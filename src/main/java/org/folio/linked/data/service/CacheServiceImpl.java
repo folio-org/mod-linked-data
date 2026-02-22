ba89cd66168ef9448f366d7071c16c98275bd138
PK Jacob <pkjacob@ebsco.com>
2026-02-22 14:47:33 -0500

    MODLD:995: Add cache clear info log in CacheService

--- src/main/java/org/folio/linked/data/service/CacheServiceImpl.java
+++ src/main/java/org/folio/linked/data/service/CacheServiceImpl.java
@@ -2,10 +2,12 @@ package org.folio.linked.data.service;
 
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class CacheServiceImpl implements CacheService {
@@ -14,6 +16,7 @@ public class CacheServiceImpl implements CacheService {
 
   @Override
   public void clearCaches() {
+    log.info("Emptying all caches");
     cacheManager.getCacheNames()
       .stream()
       .map(cacheManager::getCache)
--- src/test/java/org/folio/linked/data/service/CacheServiceImplTest.java
+++ src/test/java/org/folio/linked/data/service/CacheServiceImplTest.java
@@ -16,15 +16,17 @@ class CacheServiceImplTest {
     // given
     var cacheManager = new CaffeineCacheManager();
 
-    var specRulesCache = cacheManager.getCache("spec-rules");
+    var specRulesCache = cacheManager.getCache(SPEC_RULES);
     assertThat(specRulesCache).isNotNull();
-    specRulesCache.put(SPEC_RULES, "rule-value");
-    assertThat(specRulesCache.get(SPEC_RULES)).isNotNull();
+    var tenant1RuleKey = "tenant1-rule";
+    specRulesCache.put(tenant1RuleKey, "rule-value");
+    assertThat(specRulesCache.get(tenant1RuleKey)).isNotNull();
 
-    var profilesCache = cacheManager.getCache("profiles");
+    var profilesCache = cacheManager.getCache(PROFILES);
     assertThat(profilesCache).isNotNull();
-    profilesCache.put(PROFILES, "profile-value");
-    assertThat(profilesCache.get(PROFILES)).isNotNull();
+    var tenant1ProfileKey = "tenant1-profile";
+    profilesCache.put(tenant1ProfileKey, "profile-value");
+    assertThat(profilesCache.get(tenant1ProfileKey)).isNotNull();
 
     var cacheService = new CacheServiceImpl(cacheManager);
 
@@ -32,7 +34,7 @@ class CacheServiceImplTest {
     cacheService.clearCaches();
 
     // then
-    assertThat(specRulesCache.get(SPEC_RULES)).isNull();
-    assertThat(profilesCache.get(PROFILES)).isNull();
+    assertThat(specRulesCache.get(tenant1RuleKey)).isNull();
+    assertThat(profilesCache.get(tenant1ProfileKey)).isNull();
   }
 }
