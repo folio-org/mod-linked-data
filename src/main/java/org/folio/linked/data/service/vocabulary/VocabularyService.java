ed2b651af61cc34cb6455cb385fc8ca79c2a745a
PK Jacob <pkjacob@ebsco.com>
2026-03-05 14:44:42 -0500

    Finalize vocabulary worker and service updates

--- src/main/java/org/folio/linked/data/service/vocabulary/VocabularyService.java
+++ src/main/java/org/folio/linked/data/service/vocabulary/VocabularyService.java
@@ -3,4 +3,6 @@ package org.folio.linked.data.service.vocabulary;
 public interface VocabularyService {
 
   String getVocabularyByName(String vocabularyName);
+
+  void saveAllVocabularies();
 }
--- src/main/java/org/folio/linked/data/service/vocabulary/VocabularyServiceImpl.java
+++ src/main/java/org/folio/linked/data/service/vocabulary/VocabularyServiceImpl.java
@@ -2,7 +2,13 @@ package org.folio.linked.data.service.vocabulary;
 
 import static org.folio.linked.data.util.Constants.Cache.VOCABULARIES;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.linked.data.exception.RequestProcessingExceptionBuilder;
 import org.folio.linked.data.model.entity.VocabularyEntity;
 import org.folio.linked.data.repo.VocabularyRepository;
@@ -11,8 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class VocabularyServiceImpl implements VocabularyService {
+  private static final String VOCABULARIES_DIRECTORY = "vocabularies";
 
   private final VocabularyRepository vocabularyRepository;
   private final RequestProcessingExceptionBuilder exceptionBuilder;
@@ -24,5 +32,40 @@ public class VocabularyServiceImpl implements VocabularyService {
     return vocabularyRepository.findById(vocabularyName)
       .map(VocabularyEntity::getVocabularyJson)
       .orElseThrow(() -> exceptionBuilder.notFoundLdResourceByIdException("Vocabulary", vocabularyName));
+  }
+
+  @Override
+  @Transactional
+  public void saveAllVocabularies() {
+    var vocabulariesDirectory = getClass().getClassLoader().getResource(VOCABULARIES_DIRECTORY);
+    if (vocabulariesDirectory == null) {
+      log.warn("Vocabularies directory not found: {}", VOCABULARIES_DIRECTORY);
+      return;
+    }
+
+    try (var files = Files.list(Paths.get(vocabulariesDirectory.toURI()))) {
+      files
+        .filter(Files::isRegularFile)
+        .filter(path -> path.getFileName().toString().endsWith(".json"))
+        .forEach(this::saveVocabulary);
+    } catch (IOException | URISyntaxException | UnsupportedOperationException e) {
+      log.error("Failed to read vocabularies from directory: {}", VOCABULARIES_DIRECTORY, e);
+    }
+  }
+
+  private void saveVocabulary(Path file) {
+    try {
+      var fileName = file.getFileName().toString();
+      var vocabularyName = fileName.substring(0, fileName.length() - ".json".length());
+      var vocabularyJson = Files.readString(file);
+
+      var vocabulary = new VocabularyEntity()
+        .setVocabularyName(vocabularyName)
+        .setVocabularyJson(vocabularyJson);
+
+      vocabularyRepository.save(vocabulary);
+    } catch (IOException e) {
+      log.error("Failed to process vocabulary file: {}", file, e);
+    }
   }
 }
