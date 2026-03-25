7fa5870ea9274c61c0d6e1825620f5cfe1d36616
PK Jacob <pkjacob@ebsco.com>
2026-03-25 13:47:01 -0400

    MODLD-1026: Remove unused methods

--- src/main/java/org/folio/linked/data/service/tenant/TenantScopedExecutionService.java
+++ src/main/java/org/folio/linked/data/service/tenant/TenantScopedExecutionService.java
@@ -1,20 +1,14 @@
 package org.folio.linked.data.service.tenant;
 
-import static java.util.stream.Collectors.toMap;
 import static org.folio.linked.data.util.Constants.STANDALONE_PROFILE;
 import static org.folio.spring.tools.config.RetryTemplateConfiguration.DEFAULT_KAFKA_RETRY_TEMPLATE_NAME;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.kafka.common.header.Header;
-import org.apache.kafka.common.header.Headers;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.integration.XOkapiHeaders;
 import org.folio.spring.scope.FolioExecutionContextSetter;
@@ -25,7 +19,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.retry.RetryTemplate;
 import org.springframework.core.retry.Retryable;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -59,12 +52,6 @@ public class TenantScopedExecutionService {
       }
       return null;
     });
-  }
-
-  private FolioExecutionContext kafkaFolioExecutionContext(Headers headers) {
-    var headersMap = Arrays.stream(headers.toArray())
-      .collect(toMap(Header::key, Header::value, (o, o2) -> o2, (Supplier<Map<String, Object>>) HashMap::new));
-    return contextBuilder.forMessageHeaders(new MessageHeaders(headersMap));
   }
 
   private FolioExecutionContext tenantContext(String tenantId) {
