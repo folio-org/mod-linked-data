7bf0d3bc5ca590c949d1e449ea43b9d47affd342
pkjacob <121239864+pkjacob@users.noreply.github.com>
2026-04-02 09:43:04 -0400

    MODLD-1031: Preserve user context and headers in Kafka processing (#498)
    * MODLD-1031: Preserve user context and headers in Kafka processing
    
    This reverts commit c3afe56b66d314674a2fc148ad979ece76e7d241.
    
    * Add Token to default kafka headers

--- src/main/java/org/folio/linked/data/service/tenant/TenantScopedExecutionService.java
+++ src/main/java/org/folio/linked/data/service/tenant/TenantScopedExecutionService.java
@@ -1,14 +1,20 @@
 package org.folio.linked.data.service.tenant;
 
+import static java.util.stream.Collectors.toMap;
 import static org.folio.linked.data.util.Constants.STANDALONE_PROFILE;
+import static org.folio.spring.integration.XOkapiHeaders.TOKEN;
 import static org.folio.spring.tools.config.RetryTemplateConfiguration.DEFAULT_KAFKA_RETRY_TEMPLATE_NAME;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.integration.XOkapiHeaders;
 import org.folio.spring.scope.FolioExecutionContextSetter;
@@ -17,8 +23,10 @@ import org.folio.spring.tools.context.ExecutionContextBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.retry.RetryException;
 import org.springframework.core.retry.RetryTemplate;
 import org.springframework.core.retry.Retryable;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -35,8 +43,16 @@ public class TenantScopedExecutionService {
 
   @SneakyThrows
   public <T> T execute(String tenantId, Callable<T> job) {
-    try (var fex = new FolioExecutionContextSetter(tenantContext(tenantId))) {
+    try (var unused = new FolioExecutionContextSetter(tenantContext(tenantId))) {
       return job.call();
+    }
+  }
+
+  public <T> void executeWithRetry(Headers headers, Retryable<T> retryable, Consumer<Throwable> failureHandler) {
+    try (var unused = new FolioExecutionContextSetter(folioContextFromKafkaHeadersNoToken(headers))) {
+      retryTemplate.execute(retryable);
+    } catch (RetryException re) {
+      failureHandler.accept(re.getLastException());
     }
   }
 
@@ -52,6 +68,19 @@ public class TenantScopedExecutionService {
       }
       return null;
     });
+  }
+
+  private FolioExecutionContext folioContextFromKafkaHeadersNoToken(Headers headers) {
+    Map<String, Object> okapiHeaders = Arrays.stream(headers.toArray())
+      .filter(header -> !TOKEN.equals(header.key()))
+      .collect(toMap(
+        Header::key,
+        Header::value,
+        (existing, replacement) -> replacement,
+        HashMap::new
+      ));
+
+    return contextBuilder.forMessageHeaders(new MessageHeaders(okapiHeaders));
   }
 
   private FolioExecutionContext tenantContext(String tenantId) {
