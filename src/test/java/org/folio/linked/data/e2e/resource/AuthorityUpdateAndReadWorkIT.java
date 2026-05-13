5a50218fab8c5e40c046f4e06926e7a72cb02229
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-05-13 14:16:59 +0300

    MODLD-1059 - add integration test for authority update with same fingerprint and refactor assertions

--- src/test/java/org/folio/linked/data/e2e/resource/AuthorityUpdateAndReadWorkIT.java
+++ src/test/java/org/folio/linked/data/e2e/resource/AuthorityUpdateAndReadWorkIT.java
@@ -64,6 +64,45 @@ class AuthorityUpdateAndReadWorkIT extends ITBase {
 
 
   @Test
+  void authorityUpdate_withSameFingerprint_shouldRetainSingleAuthorityVersionAndShowItForWork() throws Exception {
+    // given
+    var authority = createAuthority();
+    var work = createWorkAndLinkToAuthority(authority);
+    var sameContentUpdateEvent = getSrsDomainEventProducerRecord(randomUUID().toString(), getAuthorityJson(),
+      SOURCE_RECORD_UPDATED, MARC_AUTHORITY);
+
+    // when
+    eventKafkaTemplate.send(sameContentUpdateEvent);
+    awaitAndAssert(() -> verify(resourceMarcService, times(2))
+      .saveMarcAuthority(any(org.folio.ld.dictionary.model.Resource.class)));
+
+    // then
+    assertWorkEdgesStillPointToAuthorityInTheDb(work, authority);
+    var authoritiesFromDb = tenantScopedExecutionService.execute(
+      TENANT_ID,
+      () -> resourceTestRepository.findAllByTypeWithEdgesLoaded(Set.of(CONCEPT.getUri(), PERSON.getUri()), 2,
+          Pageable.ofSize(10))
+        .stream()
+        .sorted(comparing(Resource::getLabel))
+        .toList()
+    );
+    assertThat(authoritiesFromDb).hasSize(1);
+    assertAuthority(authoritiesFromDb.getFirst(),
+      "bValue, aValue, cValue, qValue, dValue -- xValue -- zValue -- yValue -- vValue", true, null);
+
+    var requestBuilder = get(RESOURCE_URL + "/" + work.getId())
+      .contentType(APPLICATION_JSON)
+      .headers(defaultHeaders(env));
+    var response = mockMvc.perform(requestBuilder)
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(APPLICATION_JSON))
+      .andReturn().getResponse().getContentAsString();
+    assertThat(response)
+      .contains(authoritiesFromDb.getFirst().getId().toString())
+      .contains(authoritiesFromDb.getFirst().getLabel());
+  }
+
+  @Test
   void authorityUpdate_withNewFingerprint_shouldLinkItToPreviousAndReturnAsWorkActiveAuthority() throws Exception {
     // given
     var authority = createAuthority();
@@ -79,7 +118,7 @@ class AuthorityUpdateAndReadWorkIT extends ITBase {
 
     // then
     var authoritiesFromDb = readAndAssertAuthoritiesInTheDb();
-    assertWorkIsStillLinkedToObsoleteAuthorityInTheDb(work, authoritiesFromDb.getFirst());
+    assertWorkEdgesStillPointToAuthorityInTheDb(work, authoritiesFromDb.getFirst());
     assertGetWorkWithActiveAuthority(work.getId(), authoritiesFromDb);
   }
 
@@ -100,14 +139,14 @@ class AuthorityUpdateAndReadWorkIT extends ITBase {
       .doesNotContain(authorities.getFirst().getLabel());
   }
 
-  private void assertWorkIsStillLinkedToObsoleteAuthorityInTheDb(Resource work, Resource obsoleteAuthority) {
+  private void assertWorkEdgesStillPointToAuthorityInTheDb(Resource work, Resource authority) {
     var workFromDb = tenantScopedExecutionService.execute(
       TENANT_ID,
       () -> resourceTestRepository.findByIdWithEdgesLoaded(work.getId()).orElseThrow()
     );
     assertThat(workFromDb.getOutgoingEdges()).hasSize(3);
-    assertThat(workFromDb.getOutgoingEdges()).contains(new ResourceEdge(workFromDb, obsoleteAuthority, AUTHOR));
-    assertThat(workFromDb.getOutgoingEdges()).contains(new ResourceEdge(workFromDb, obsoleteAuthority, CREATOR));
+    assertThat(workFromDb.getOutgoingEdges()).contains(new ResourceEdge(workFromDb, authority, AUTHOR));
+    assertThat(workFromDb.getOutgoingEdges()).contains(new ResourceEdge(workFromDb, authority, CREATOR));
   }
 
   private List<Resource> readAndAssertAuthoritiesInTheDb() {
