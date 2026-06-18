17bdf815fad25002b44e32aae6b0f54db4865ccc
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-06-18 13:00:23 +0300

    MODLD-1082 - test for identifier types

--- src/test/java/org/folio/linked/data/e2e/mappings/authority/TemporalAuthorityIT.java
+++ src/test/java/org/folio/linked/data/e2e/mappings/authority/TemporalAuthorityIT.java
@@ -2,6 +2,8 @@ package org.folio.linked.data.e2e.mappings.authority;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.linked.data.test.TestUtil.AUTHORITY_PATH;
+import static org.folio.linked.data.test.TestUtil.TEST_JSON_MAPPER;
+import static org.folio.linked.data.test.TestUtil.getOutgoingResources;
 import static org.folio.linked.data.test.TestUtil.getProperty;
 import static org.folio.linked.data.test.TestUtil.validateResourceType;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -20,7 +22,19 @@ class TemporalAuthorityIT extends PostResourceIT {
         "resource": {
           "_authority": {
             "profileId": 15,
-            "http://bibfra.me/vocab/lite/name": ["Test Temporal Name"]
+            "http://bibfra.me/vocab/lite/name": ["Test Temporal Name"],
+            "http://library.link/vocab/map": [
+              {
+                "http://library.link/identifier/LCCN": {
+                  "http://bibfra.me/vocab/lite/name": ["n2024001234"]
+                }
+              },
+              {
+                "http://library.link/identifier/UNKNOWN": {
+                  "http://bibfra.me/vocab/lite/name": ["other-id-123"]
+                }
+              }
+            ]
           }
         }
       }""";
@@ -32,6 +46,14 @@ class TemporalAuthorityIT extends PostResourceIT {
     apiResponse
       .andExpect(jsonPath(AUTHORITY_PATH + "['profileId']").value(15))
       .andExpect(jsonPath(AUTHORITY_PATH + "['http://bibfra.me/vocab/lite/name'][0]").value("Test Temporal Name"));
+
+    var responsePayload = apiResponse.andReturn().getResponse().getContentAsString();
+    var mapNode = TEST_JSON_MAPPER.readTree(responsePayload)
+      .path("resource").path("_authority").path("http://library.link/vocab/map");
+    assertThat(mapNode.get(0).path("http://library.link/identifier/LCCN")
+      .path("http://bibfra.me/vocab/lite/name").get(0).asString()).isEqualTo("n2024001234");
+    assertThat(mapNode.get(1).path("http://library.link/identifier/UNKNOWN")
+      .path("http://bibfra.me/vocab/lite/name").get(0).asString()).isEqualTo("other-id-123");
   }
 
   @Override
@@ -39,5 +61,22 @@ class TemporalAuthorityIT extends PostResourceIT {
     validateResourceType(resource, "http://bibfra.me/vocab/lite/Temporal");
     assertThat(getProperty(resource, "http://bibfra.me/vocab/lite/name")).isEqualTo("Test Temporal Name");
     assertThat(resource.getLabel()).isEqualTo("Test Temporal Name");
+
+    var identifiers = getOutgoingResources(resource, "http://library.link/vocab/map");
+    assertThat(identifiers).hasSize(2);
+
+    var lccn = identifiers.stream()
+      .filter(r -> r.getTypes().stream().anyMatch(t -> "http://library.link/identifier/LCCN".equals(t.getUri())))
+      .findFirst().orElseThrow();
+    validateResourceType(lccn, "http://bibfra.me/vocab/lite/Identifier", "http://library.link/identifier/LCCN");
+    assertThat(getProperty(lccn, "http://bibfra.me/vocab/lite/name")).isEqualTo("n2024001234");
+    assertThat(lccn.getLabel()).isEqualTo("n2024001234");
+
+    var otherId = identifiers.stream()
+      .filter(r -> r.getTypes().stream().anyMatch(t -> "http://library.link/identifier/UNKNOWN".equals(t.getUri())))
+      .findFirst().orElseThrow();
+    validateResourceType(otherId, "http://bibfra.me/vocab/lite/Identifier", "http://library.link/identifier/UNKNOWN");
+    assertThat(getProperty(otherId, "http://bibfra.me/vocab/lite/name")).isEqualTo("other-id-123");
+    assertThat(otherId.getLabel()).isEqualTo("other-id-123");
   }
 }
