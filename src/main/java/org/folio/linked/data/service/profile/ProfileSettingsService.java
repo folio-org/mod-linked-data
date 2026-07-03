9581f7dfafe3bf9fd84f4fd46439e2c3c3fc67cb
Ryan Lee <rlee1@ebsco.com>
2026-07-03 00:45:27 -0700

    MODLD-1040: Update profile settings model to include multiple settings per profile and a label (#546)
    * Add changes to support multiple named settings per user per profile
    
    * Update API specs to work with modified data model
    
    * Update methods to work with modified data model and modified API
    
    * Add create settings endpoint, update integration tests
    
    * Test setProfileSettings
    
    * Return saved object, add a delete method
    
    * Update tests
    
    * Pull out profile existence check to its own method
    
    * Require unique profile setting names per user-profile
    
    * Remove trailing spaces
    
    * Add NotBlank annotation to profile settings name
    
    * Add validator unit test, update integration test with constraint tests
    
    * Update changelog
    
    * Remove stray semi
    
    * Update module descriptor with profile settings changes

--- src/main/java/org/folio/linked/data/service/profile/ProfileSettingsService.java
+++ src/main/java/org/folio/linked/data/service/profile/ProfileSettingsService.java
@@ -1,10 +1,27 @@
 package org.folio.linked.data.service.profile;
 
+import java.util.List;
+import org.folio.linked.data.domain.dto.CustomProfileSettingsMetadata;
 import org.folio.linked.data.domain.dto.CustomProfileSettingsRequestDto;
 import org.folio.linked.data.domain.dto.CustomProfileSettingsResponseDto;
 
 public interface ProfileSettingsService {
-  CustomProfileSettingsResponseDto getProfileSettings(Integer profileId);
+  List<CustomProfileSettingsMetadata> getAllProfileSettings(Integer profileId);
 
-  void setProfileSettings(Integer profileId, CustomProfileSettingsRequestDto profileSettingsRequest);
+  CustomProfileSettingsResponseDto getProfileSettings(Integer profileId, Integer profileSettingsId);
+
+  CustomProfileSettingsMetadata createProfileSettings(
+    Integer profileId,
+    CustomProfileSettingsRequestDto profileSettingsRequest
+  );
+
+  void setProfileSettings(
+    Integer profileId,
+    Integer profileSettingsId,
+    CustomProfileSettingsRequestDto profileSettingsRequest
+  );
+
+  void deleteProfileSettings(Integer profileId, Integer profileSettingsId);
+
+  Boolean nameExistsForProfile(Integer profileId, CustomProfileSettingsRequestDto profileSettingsRequest);
 }
