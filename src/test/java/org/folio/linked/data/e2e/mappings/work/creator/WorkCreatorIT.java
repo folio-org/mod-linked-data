99491987655c51f2a5b26f2450eb06d4a508fea5
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-07-08 16:52:50 +0300

    MODLD-1107 - fix checkstyle

--- src/test/java/org/folio/linked/data/e2e/mappings/work/creator/WorkCreatorIT.java
+++ src/test/java/org/folio/linked/data/e2e/mappings/work/creator/WorkCreatorIT.java
@@ -2,14 +2,13 @@ package org.folio.linked.data.e2e.mappings.work.creator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.folio.ld.dictionary.PropertyDictionary.NAME;
 import static org.folio.linked.data.test.TestUtil.getFirstOutgoingResource;
+import static org.folio.linked.data.test.TestUtil.getJsonNode;
 import static org.folio.linked.data.test.TestUtil.getOutgoingResources;
 import static org.folio.linked.data.test.TestUtil.validateResourceType;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import static org.folio.ld.dictionary.PropertyDictionary.NAME;
-import static org.folio.linked.data.test.TestUtil.getJsonNode;
 
 import java.util.List;
 import java.util.Map;
